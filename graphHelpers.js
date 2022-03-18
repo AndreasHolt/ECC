57a53da40cbe14af9256bdc71e70b2ffa0cd4dd8
AndreasHolt <holt.andreas@gmail.com>
2022-03-18 14:30:14 +0100

    extra thicc

--- graphHelpers.js
+++ graphHelpers.js
@@ -44,26 +44,62 @@ Graph.prototype.addCalculatedPoint = function(x, y) {
 	if(document.getElementsByClassName('calculatedPoints').length == 2){
 		document.getElementsByClassName('calculatedPoints')[1].remove();
 		document.getElementsByClassName('calculatedPoints')[0].remove();
+        
+		document.getElementsByClassName('linesConnecting')[1].remove();
+		document.getElementsByClassName('linesConnecting')[0].remove();
 	}
     var svgNS = "http://www.w3.org/2000/svg";
 
     let arrayIntersectInverted = [y, -y]
 
     for(i = 0; i < arrayIntersectInverted.length; i++){
-        console.log('test')
-     var circle = document.createElementNS(svgNS,'circle');
-     (i == 0)?(circle.setAttribute('fill','dodgerblue')):(circle.setAttribute('fill','fuchsia'));
+         var circle = document.createElementNS(svgNS,'circle');
+         (i == 0)?(circle.setAttribute('fill','dodgerblue')):(circle.setAttribute('fill','fuchsia'));
 
-     circle.setAttribute('cx', (x * this.scaleX) + this.centerX);
-     circle.setAttribute('cy', (-arrayIntersectInverted[i] * this.scaleY) + this.centerY);
-     circle.classList.add('calculatedPoints')
-     circle.setAttribute('r',5);
+         circle.setAttribute('cx', (x * this.scaleX) + this.centerX);
+         circle.setAttribute('cy', (-arrayIntersectInverted[i] * this.scaleY) + this.centerY);
+         circle.classList.add('calculatedPoints')
+         circle.setAttribute('r', 5);
 
-     var svg = document.querySelector('svg');
-     svg.appendChild(circle);
+         var svg = document.querySelector('svg');
+         svg.appendChild(circle);
+
+        let fromPoint = document.getElementsByClassName('workingPoints')[0]
+
+        var newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
+        if(i == 0){
+            newLine.setAttribute('x1',fromPoint.getAttribute('cx'));
+            newLine.setAttribute('y1',fromPoint.getAttribute('cy'));
+
+            newLine.classList.add('linesConnecting')
+            newLine.setAttribute('x2', (x * this.scaleX) + this.centerX);
+            newLine.setAttribute('y2', (arrayIntersectInverted[i+1] * this.scaleY) + this.centerY);
+            newLine.setAttribute("stroke", "dodgerblue")
+            newLine.setAttribute("stroke-width", "2")
+            svg.appendChild(newLine);
+        } else {
+            newLine.setAttribute('x1', (x * this.scaleX) + this.centerX);
+            newLine.setAttribute('y1', (arrayIntersectInverted[i] * this.scaleY) + this.centerY);
+ 
+            newLine.classList.add('linesConnecting')
+            newLine.setAttribute('x2', (x * this.scaleX) + this.centerX);
+            newLine.setAttribute('y2', (arrayIntersectInverted[i-1] * this.scaleY) + this.centerY);
+            newLine.setAttribute("stroke", "fuchsia")
+            newLine.setAttribute("stroke-width", "2")
+            svg.appendChild(newLine);
+        }
+
+        
+
+
+
+
+
+
+
 
     }
-    }
+}
 
 Graph.prototype.addPointOnClick = function() {
     let point = document.getElementById('point')
