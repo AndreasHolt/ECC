96d0c05a3925513f32a836407648f389df112299
MikkelBjørn <mikkel.bjoern@hotmail.com>
2022-03-22 11:08:40 +0100

    Old commits

--- graphHelpers.js
+++ graphHelpers.js
@@ -41,6 +41,23 @@ Graph.prototype.coordsToGraph = function(coordsX, coordsY) {
 	return {x, y}
 }
 
+Graph.prototype.getPointPlacement = function(x) {
+    let point1 = document.getElementsByClassName('workingPoints')[0];
+    let point2 = document.getElementsByClassName('workingPoints')[1]
+    let x1 = (point1.getAttribute('cx')-this.centerX)/this.scaleX;
+    let x2 = (point2.getAttribute('cx')-this.centerX)/this.scaleX;
+    let returnValue;
+
+    if((x > x1) && (x > x2)) {
+        returnValue = 1;
+    } else if( (x1 >= x && x2 <= x) || (x1 <= x && x2 >= x) ) {
+        returnValue = 2;
+    } else if(x1 >= x && x2 >= x) {
+        returnValue = 3;
+    }
+    return returnValue;
+}
+
 Graph.prototype.addCalculatedPoint = function(x, y) {
 	if(document.getElementsByClassName('calculatedPoints').length == 2){
 		document.getElementsByClassName('calculatedPoints')[1].remove();
@@ -73,23 +90,56 @@ Graph.prototype.addCalculatedPoint = function(x, y) {
     }
 }
 
-Graph.prototype.drawLine = function(operator, color, i, x, y, svg) {
-        let fromPoint = document.getElementsByClassName('workingPoints')[0]
-        var newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
-        if(i == 0){
-            newLine.setAttribute('x1',fromPoint.getAttribute('cx'));
-            newLine.setAttribute('y1',fromPoint.getAttribute('cy'));
+function logicPointAddition(x) {
+    let pointDecider = myGraph.getPointPlacement(x);
+    let fromPoint = 0;
+    if(pointDecider === 1) {
+        let tempPoint = document.getElementsByClassName('workingPoints')
+       
+        if(tempPoint[0].getAttribute('cx') > tempPoint[1].getAttribute('cx')) {
+            fromPoint = tempPoint[1];
         } else {
-            newLine.setAttribute('x1', (x * this.scaleX) + this.centerX);
-            newLine.setAttribute('y1', (-y * this.scaleY) + this.centerY);
- 
+            fromPoint = tempPoint[0];
         }
-        newLine.classList.add('linesConnecting')
+    } else if(pointDecider === 2) {
+        fromPoint = document.getElementsByClassName('workingPoints')[0]
+    } else if(pointDecider === 3) {
+        let tempPoint = document.getElementsByClassName('workingPoints')
+        
+        if(tempPoint[0].getAttribute('cx') > tempPoint[1].getAttribute('cx')) {
+            fromPoint = tempPoint[0];
+        } else {
+            fromPoint = tempPoint[1];
+        }
+    }
+    return fromPoint;
+}
+
+Graph.prototype.drawLine = function(operator, color, i, x, y, svg) {
+    let fromPoint = logicPointAddition(x);
+    let pointDecider = myGraph.getPointPlacement(x);
+
+    var newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
+    if(i == 0){
+        newLine.setAttribute('x1',fromPoint.getAttribute('cx'));
+        newLine.setAttribute('y1',fromPoint.getAttribute('cy'));
+    } else {
+        newLine.setAttribute('x1', (x * this.scaleX) + this.centerX);
+        newLine.setAttribute('y1', (-y * this.scaleY) + this.centerY);
+
+    }
+    if(pointDecider === 2 && i == 0) {
+        let secondPoint = document.getElementsByClassName('workingPoints')[1]
+        newLine.setAttribute('x2', secondPoint.getAttribute('cx'));
+        newLine.setAttribute('y2', secondPoint.getAttribute('cy'));
+    } else {
         newLine.setAttribute('x2', (x * this.scaleX) + this.centerX);
         newLine.setAttribute('y2', ((((operator == '-')?(y):(y)) * this.scaleY) + this.centerY));
-        newLine.setAttribute("stroke", color)
-        newLine.setAttribute("stroke-width", "2")
-        svg.appendChild(newLine);
+    }
+    newLine.classList.add('linesConnecting')
+    newLine.setAttribute("stroke", color)
+    newLine.setAttribute("stroke-width", "2")
+    svg.appendChild(newLine);
 }
 
 Graph.prototype.addPointOnClick = function() {
