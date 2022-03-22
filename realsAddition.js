1c751bc2fc85f68c3a16adbe6afd924655df9aa1
MikkelBjørn <mikkel.bjoern@hotmail.com>
2022-03-22 09:55:36 +0100

    Old commits

--- realsAddition.js
+++ realsAddition.js
@@ -5,7 +5,10 @@ function pointAddition() {
         point1: [points[0].getAttribute('cx'), points[0].getAttribute('cy')],
         point2: [points[1].getAttribute('cx'), points[1].getAttribute('cy')]
     }
+    let x1 = storePoints.point1[0], y1 = storePoints.point1[1];
+    let x2 = storePoints.point2[0], y2 = storePoints.point2[1];
 
-    let lambda = ((storePoints.point2[1] - storePoints.point1[1]) / (storePoints.point2[0] - storePoints.point1[0]))
-    let thirdX = lambda * lambda  + lambda // *
+    let lambda = ((y2 - y1) / (x2 - x1));
+    let newX = lambda * lambda  - x2 - x1;
+    let newY = -y2 + lambda*(x2-newX);
 }
