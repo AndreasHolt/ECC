e7ac2eb05883fe2cffa94058070dcecb0ea0acf5
Lucas Frendorf <lucasfrendorf@gmail.com>
2022-03-25 13:42:35 +0100

    Old commits

--- realsAddition.js
+++ realsAddition.js
@@ -1,18 +1,18 @@
 Graph.prototype.pointAddition = function () {
-  const points = document.getElementsByClassName('workingPoints');
+    const points = document.getElementsByClassName('workingPoints');
 
-  storePoints = {
-    point1: [points[0].getAttribute('cx'), points[0].getAttribute('cy')],
-    point2: [points[1].getAttribute('cx'), points[1].getAttribute('cy')],
-  };
-  const x1 = (storePoints.point1[0] - this.centerX) / this.scaleX; const
-    y1 = (storePoints.point1[1] - this.centerY) / this.scaleY;
-  const x2 = (storePoints.point2[0] - this.centerX) / this.scaleX; const
-    y2 = (storePoints.point2[1] - this.centerY) / this.scaleY;
+    storePoints = {
+        point1: [points[0].getAttribute('cx'), points[0].getAttribute('cy')],
+        point2: [points[1].getAttribute('cx'), points[1].getAttribute('cy')],
+    };
+    const x1 = (storePoints.point1[0] - this.centerX) / this.scaleX; const
+        y1 = (storePoints.point1[1] - this.centerY) / this.scaleY;
+    const x2 = (storePoints.point2[0] - this.centerX) / this.scaleX; const
+        y2 = (storePoints.point2[1] - this.centerY) / this.scaleY;
 
-  const lambda = ((y2 - y1) / (x2 - x1));
-  const newX = (lambda * lambda) - x2 - x1;
-  const newY = y2 + lambda * newX + lambda * (-x2);
+    const lambda = ((y2 - y1) / (x2 - x1));
+    const newX = (lambda * lambda) - x2 - x1;
+    const newY = y2 + lambda * newX + lambda * (-x2);
 
-  myGraph.addCalculatedPoint(newX, newY, 1);
+    myGraph.addCalculatedPoint(newX, newY, 1);
 };
--- script.js
+++ script.js
@@ -1,44 +1,6 @@
 let scaleZoom = 10;
 
 let myGraph = new Graph({
-  canvasId: 'myCanvas',
-  minX: -scaleZoom,
-  minY: -scaleZoom,
-  maxX: scaleZoom,
-  maxY: scaleZoom,
-  parameterA: -5,
-  parameterB: 15,
-  unitsPerTick: scaleZoom / 5,
-});
-
-Graph.prototype.equationP = function (x) {
-  return Math.sqrt((x * x * x) + this.parameterA * x + this.parameterB);
-};
-
-function drawEquation() {
-  myGraph.drawEquation((x) => myGraph.equationP(x), 'green', 3);
-
-  myGraph.drawEquation((x) => -myGraph.equationP(x), 'green', 3);
-}
-
-drawEquation();
-
-document.getElementById('pointSVG').addEventListener('wheel', (e) => {
-  e.preventDefault();
-  let graphPos; let
-    graphPos2;
-
-  e.preventDefault(); // Prevents page scroll when zooming
-
-  myGraph.context.clearRect(0, 0, 578, 300); // Use var of size instead
-
-  if (e.deltaY < 0) { // Zoom in
-    scaleZoom /= 1.02;
-  } else { // Zoom out
-    scaleZoom *= 1.02;
-  }
-
-  myGraph = new Graph({
     canvasId: 'myCanvas',
     minX: -scaleZoom,
     minY: -scaleZoom,
@@ -47,139 +9,177 @@ document.getElementById('pointSVG').addEventListener('wheel', (e) => {
     parameterA: -5,
     parameterB: 15,
     unitsPerTick: scaleZoom / 5,
-  });
+});
 
-  myGraph.drawXAxis();
-  myGraph.drawYAxis();
+Graph.prototype.equationP = function (x) {
+    return Math.sqrt((x * x * x) + this.parameterA * x + this.parameterB);
+};
 
-  drawEquation();
+function drawEquation() {
+    myGraph.drawEquation((x) => myGraph.equationP(x), 'green', 3);
 
-  const points = document.querySelectorAll('.workingPoints,.calculatedPoints,.point');
+    myGraph.drawEquation((x) => -myGraph.equationP(x), 'green', 3);
+}
 
-  // TODO when scrolling in and out the "point" is not at the cursor y pos
-  for (let i = 0; i < points.length; i++) {
-    const el = points[i];
+drawEquation();
 
-    const cx = el.getAttribute('cx');
-    const cy = el.getAttribute('cy');
-    const coords = myGraph.graphToCoords(cx, cy);
+document.getElementById('pointSVG').addEventListener('wheel', (e) => {
+    e.preventDefault();
+    let graphPos; let
+        graphPos2;
 
-    if (e.deltaY < 0) { // Zoom in
-      graphPos = myGraph.coordsToGraph(coords.x * 1.02, coords.y * 1.02);
-    } else { // Zoom out
-      graphPos = myGraph.coordsToGraph(coords.x / 1.02, coords.y / 1.02);
-    }
+    e.preventDefault(); // Prevents page scroll when zooming
 
-    el.setAttribute('cx', graphPos.x);
-    el.setAttribute('cy', graphPos.y);
-  }
-
-  const lines = document.getElementsByClassName('linesConnecting');
-
-  for (let i = 0; i < lines.length; i++) {
-    const el = lines[i];
-
-    const x1 = el.getAttribute('x1');
-    const y1 = el.getAttribute('y1');
-    const x2 = el.getAttribute('x2');
-    const y2 = el.getAttribute('y2');
-
-    const coords1 = myGraph.graphToCoords(x1, y1);
-    const coords2 = myGraph.graphToCoords(x2, y2);
+    myGraph.context.clearRect(0, 0, 578, 300); // Use var of size instead
 
     if (e.deltaY < 0) { // Zoom in
-      graphPos = myGraph.coordsToGraph(coords1.x * 1.02, coords1.y * 1.02);
-      graphPos2 = myGraph.coordsToGraph(coords2.x * 1.02, coords2.y * 1.02);
+        scaleZoom /= 1.02;
     } else { // Zoom out
-      graphPos = myGraph.coordsToGraph(coords1.x / 1.02, coords1.y / 1.02);
-      graphPos2 = myGraph.coordsToGraph(coords2.x / 1.02, coords2.y / 1.02);
+        scaleZoom *= 1.02;
     }
 
-    el.setAttribute('x1', graphPos.x);
-    el.setAttribute('y1', graphPos.y);
-    el.setAttribute('x2', graphPos2.x);
-    el.setAttribute('y2', graphPos2.y);
-  }
+    myGraph = new Graph({
+        canvasId: 'myCanvas',
+        minX: -scaleZoom,
+        minY: -scaleZoom,
+        maxX: scaleZoom,
+        maxY: scaleZoom,
+        parameterA: -5,
+        parameterB: 15,
+        unitsPerTick: scaleZoom / 5,
+    });
+
+    myGraph.drawXAxis();
+    myGraph.drawYAxis();
+
+    drawEquation();
+
+    const points = document.querySelectorAll('.workingPoints,.calculatedPoints,.point');
+
+    // TODO when scrolling in and out the "point" is not at the cursor y pos
+    for (let i = 0; i < points.length; i++) {
+        const el = points[i];
+
+        const cx = el.getAttribute('cx');
+        const cy = el.getAttribute('cy');
+        const coords = myGraph.graphToCoords(cx, cy);
+
+        if (e.deltaY < 0) { // Zoom in
+            graphPos = myGraph.coordsToGraph(coords.x * 1.02, coords.y * 1.02);
+        } else { // Zoom out
+            graphPos = myGraph.coordsToGraph(coords.x / 1.02, coords.y / 1.02);
+        }
+
+        el.setAttribute('cx', graphPos.x);
+        el.setAttribute('cy', graphPos.y);
+    }
+
+    const lines = document.getElementsByClassName('linesConnecting');
+
+    for (let i = 0; i < lines.length; i++) {
+        const el = lines[i];
+
+        const x1 = el.getAttribute('x1');
+        const y1 = el.getAttribute('y1');
+        const x2 = el.getAttribute('x2');
+        const y2 = el.getAttribute('y2');
+
+        const coords1 = myGraph.graphToCoords(x1, y1);
+        const coords2 = myGraph.graphToCoords(x2, y2);
+
+        if (e.deltaY < 0) { // Zoom in
+            graphPos = myGraph.coordsToGraph(coords1.x * 1.02, coords1.y * 1.02);
+            graphPos2 = myGraph.coordsToGraph(coords2.x * 1.02, coords2.y * 1.02);
+        } else { // Zoom out
+            graphPos = myGraph.coordsToGraph(coords1.x / 1.02, coords1.y / 1.02);
+            graphPos2 = myGraph.coordsToGraph(coords2.x / 1.02, coords2.y / 1.02);
+        }
+
+        el.setAttribute('x1', graphPos.x);
+        el.setAttribute('y1', graphPos.y);
+        el.setAttribute('x2', graphPos2.x);
+        el.setAttribute('y2', graphPos2.y);
+    }
 });
 
 /// ----------------------------------------------------------------------
 /// Draw points on graph
 /// ----------------------------------------------------------------------
 function deletePoints() {
-  const allSVG = [
-    document.getElementsByClassName('workingPoints'),
-    document.getElementsByClassName('linesConnecting'),
-    document.getElementsByClassName('calculatedPoints'),
-  ];
+    const allSVG = [
+        document.getElementsByClassName('workingPoints'),
+        document.getElementsByClassName('linesConnecting'),
+        document.getElementsByClassName('calculatedPoints'),
+    ];
 
-  for (const key of allSVG) {
-    for (let i = key.length; i > 0; i--) {
-      // console.log(key[i-1])
-      key[i - 1].remove();
+    for (const key of allSVG) {
+        for (let i = key.length; i > 0; i--) {
+            // console.log(key[i-1])
+            key[i - 1].remove();
+        }
     }
-  }
 }
 
 document.getElementById('pointSVG').addEventListener('mousemove', (e) => {
-  myGraph.movePoint(e);
+    myGraph.movePoint(e);
 });
 
 const operations = document.getElementsByClassName('operation');
 
 function init() {
-  for (const input of operations) {
-    input.addEventListener('click', (e) => {
-      for (const buttons of operations) {
-        if (buttons.disabled == true) {
-          buttons.disabled = false;
-        }
-      }
+    for (const input of operations) {
+        input.addEventListener('click', (e) => {
+            for (const buttons of operations) {
+                if (buttons.disabled == true) {
+                    buttons.disabled = false;
+                }
+            }
 
-      deletePoints();
+            deletePoints();
 
-      e.target.disabled = true;
-    });
-  }
+            e.target.disabled = true;
+        });
+    }
 }
 
 init();
 
 document.getElementById('layer2').addEventListener('click', (e) => {
-  const pointsOnGraph = document.getElementsByClassName('workingPoints');
+    const pointsOnGraph = document.getElementsByClassName('workingPoints');
 
-  // Delete the point on the graph that was placed first
-  if (operations[0].disabled) {
-    if (pointsOnGraph.length === 1) {
-      myGraph.addPointOnClick();
-      runOperation(1);
-    } else if (pointsOnGraph.length === 0) {
-      myGraph.addPointOnClick();
-    } else {
-      deletePoints();
+    // Delete the point on the graph that was placed first
+    if (operations[0].disabled) {
+        if (pointsOnGraph.length === 1) {
+            myGraph.addPointOnClick();
+            runOperation(1);
+        } else if (pointsOnGraph.length === 0) {
+            myGraph.addPointOnClick();
+        } else {
+            deletePoints();
+        }
+    } else if (operations[1].disabled) {
+        if (pointsOnGraph.length === 0) {
+            myGraph.addPointOnClick();
+            runOperation(2);
+        } else if (pointsOnGraph.length === 1) {
+            deletePoints();
+        }
     }
-  } else if (operations[1].disabled) {
-    if (pointsOnGraph.length === 0) {
-      myGraph.addPointOnClick();
-      runOperation(2);
-    } else if (pointsOnGraph.length === 1) {
-      deletePoints();
-    }
-  }
 });
 
 function runOperation(operations) {
-  switch (operations) {
+    switch (operations) {
     case 1:
-      myGraph.pointAddition();
-      break;
+        myGraph.pointAddition();
+        break;
     case 2:
-      myGraph.pointDouble();
-      break;
+        myGraph.pointDouble();
+        break;
     case 3:
-      console.log('Hey Chat!');
-      break;
+        console.log('Hey Chat!');
+        break;
     default:
-      console.log('Please no');
-      break;
-  }
+        console.log('Please no');
+        break;
+    }
 }
