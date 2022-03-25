097d150b9bd67e53a56a0a9da6fa110008cbd93e
Lucas Frendorf <lucasfrendorf@gmail.com>
2022-03-25 13:40:02 +0100

    Old commits

--- realsAddition.js
+++ realsAddition.js
@@ -1,16 +1,18 @@
-Graph.prototype.pointAddition = function() {
-    let points = document.getElementsByClassName('workingPoints')
-    
-	storePoints = {
-        point1: [points[0].getAttribute('cx'), points[0].getAttribute('cy')],
-        point2: [points[1].getAttribute('cx'), points[1].getAttribute('cy')]
-    }
-    let x1 = (storePoints.point1[0]-this.centerX)/this.scaleX, y1 = (storePoints.point1[1]-this.centerY)/this.scaleY;
-    let x2 = (storePoints.point2[0]-this.centerX)/this.scaleX, y2 = (storePoints.point2[1]-this.centerY)/this.scaleY;
+Graph.prototype.pointAddition = function () {
+  const points = document.getElementsByClassName('workingPoints');
 
-    let lambda = ((y2 - y1) / (x2 - x1));
-    let newX = (lambda * lambda) - x2 - x1;
-    let newY = y2 + lambda*newX + lambda*(-x2);
+  storePoints = {
+    point1: [points[0].getAttribute('cx'), points[0].getAttribute('cy')],
+    point2: [points[1].getAttribute('cx'), points[1].getAttribute('cy')],
+  };
+  const x1 = (storePoints.point1[0] - this.centerX) / this.scaleX; const
+    y1 = (storePoints.point1[1] - this.centerY) / this.scaleY;
+  const x2 = (storePoints.point2[0] - this.centerX) / this.scaleX; const
+    y2 = (storePoints.point2[1] - this.centerY) / this.scaleY;
 
-    myGraph.addCalculatedPoint(newX, newY, 1);
-}
+  const lambda = ((y2 - y1) / (x2 - x1));
+  const newX = (lambda * lambda) - x2 - x1;
+  const newY = y2 + lambda * newX + lambda * (-x2);
+
+  myGraph.addCalculatedPoint(newX, newY, 1);
+};
