30cd92105566bddb87da96051c4a7b7c8534d3ba
Lucas Frendorf <lucasfrendorf@gmail.com>
2022-03-25 13:42:35 +0100

    Old commits

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-  },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        indent: ['error', 4],
+    },
 };
--- realsDoubling.js
+++ realsDoubling.js
@@ -1,15 +1,15 @@
 Graph.prototype.pointDouble = function () {
-  const points = document.getElementsByClassName('workingPoints');
-  storePoints = {
-    point1: [points[0].getAttribute('cx'), points[0].getAttribute('cy')],
-  };
+    const points = document.getElementsByClassName('workingPoints');
+    storePoints = {
+        point1: [points[0].getAttribute('cx'), points[0].getAttribute('cy')],
+    };
 
-  const x = (storePoints.point1[0] - this.centerX) / this.scaleX;
-  const y = -(storePoints.point1[1] - this.centerY) / this.scaleY;
+    const x = (storePoints.point1[0] - this.centerX) / this.scaleX;
+    const y = -(storePoints.point1[1] - this.centerY) / this.scaleY;
 
-  const lambda = (3 * x * x + this.parameterA) / (2 * y);
-  const newX = lambda * lambda - 2 * x;
-  const newY = -y + lambda * (x - newX);
+    const lambda = (3 * x * x + this.parameterA) / (2 * y);
+    const newX = lambda * lambda - 2 * x;
+    const newY = -y + lambda * (x - newX);
 
-  myGraph.addCalculatedPoint(newX, newY, 2);
+    myGraph.addCalculatedPoint(newX, newY, 2);
 };
