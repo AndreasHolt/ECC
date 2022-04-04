3377ca0e46cb0ade9ec7e1ea9e979b37e5f821e6
Lucas Frendorf <lucasfrendorf@gmail.com>
2022-04-04 15:48:37 +0200

    More rewrites

--- src/js/realsMultiplication.js
+++ src/js/realsMultiplication.js
@@ -42,40 +42,4 @@ function pointMultiplication(myGraph) {
     return res;
 }
 
-/*
-function pointAddition(myGraph) {
-    const points = document.getElementsByClassName('workingPoints');
-
-    const storePoints = {
-        point1: [points[0].getAttribute('cx'), points[0].getAttribute('cy')],
-        point2: [points[1].getAttribute('cx'), points[1].getAttribute('cy')],
-    };
-    const x1 = (storePoints.point1[0] - myGraph.centerX) / myGraph.scaleX; const
-        y1 = (storePoints.point1[1] - myGraph.centerY) / myGraph.scaleY;
-    const x2 = (storePoints.point2[0] - myGraph.centerX) / myGraph.scaleX; const
-        y2 = (storePoints.point2[1] - myGraph.centerY) / myGraph.scaleY;
-
-    const lambda = ((y2 - y1) / (x2 - x1));
-
-    let newX = (lambda * lambda) - x2 - x1;
-    let newY = 0;
-
-    // Handle edge case: same x coordinate for both points (i.e. vertical line), but not same y coordinate
-    if (x2 === x1 && y1 !== y2) {
-        newY = 9999999;
-        newX = x1;
-    } else if (x2 === x1 && y1 === y2) {
-        pointDouble(myGraph); // Handle edge case: both points are the same, so double the point instead
-
-        // TODO: pointDoublingSteps when implemented
-        return;
-    } else {
-        newY = y2 + lambda * newX + lambda * (-x2);
-    }
-
-    pointAdditionSteps(myGraph, listedPoints, lambda, newX, newY);
-
-    addCalculatedPoint(myGraph, newX, newY, 1);
-}
-*/
 export { pointMultiplication };
