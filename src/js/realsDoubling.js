b79a76e6af0aa39af95e0d63f90bd88611c256ab
Lucas Frendorf <lucasfrendorf@gmail.com>
2022-04-04 10:38:06 +0200

    Old commits

--- src/js/realsDoubling.js
+++ src/js/realsDoubling.js
@@ -1,19 +1,33 @@
 import { addCalculatedPoint } from './graphHelpers';
 
 function pointDouble(myGraph) {
+    const pointArr = [];
+    let newPointArr = [];
+
     const points = document.getElementsByClassName('workingPoints');
     const storePoints = {
         point1: [points[0].getAttribute('cx'), points[0].getAttribute('cy')],
     };
 
-    const x = (storePoints.point1[0] - myGraph.centerX) / myGraph.scaleX;
-    const y = -(storePoints.point1[1] - myGraph.centerY) / myGraph.scaleY;
+    pointArr[0] = (storePoints.point1[0] - myGraph.centerX) / myGraph.scaleX;
+    pointArr[1] = -(storePoints.point1[1] - myGraph.centerY) / myGraph.scaleY;
 
-    const lambda = (3 * x * x + myGraph.parameterA) / (2 * y);
-    const newX = lambda * lambda - 2 * x;
-    const newY = -y + lambda * (x - newX);
+    newPointArr = calculateDouble(myGraph, pointArr); 
 
-    addCalculatedPoint(myGraph, newX, newY, 2);
+    addCalculatedPoint(myGraph, newPointArr[0], newPointArr[1], 2);
 }
 
-export { pointDouble };
+function calculateDouble(myGraph, point) {
+    const newPointArr = [];
+
+    const lambda = (3 * point[0] * point[0] + myGraph.parameterA) / (2 * point[1]);
+    const newX = lambda * lambda - 2 * point[0];
+    const newY = -point[1] + lambda * (point[0] - newX);
+
+    newPointArr[0] = newX;
+    newPointArr[1] = newY;
+
+    return newPointArr;
+}
+
+export { pointDouble, calculateDouble };
