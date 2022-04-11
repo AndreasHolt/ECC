36cb0691f8eb4439ece1105fff34a4840a5f0c34
AndreasHolt <aholt21@student.aau.dk>
2022-04-11 13:40:28 +0200

    Old commits

--- src/js/realsMultiplication.js
+++ src/js/realsMultiplication.js
@@ -21,7 +21,7 @@ function pointMultiplicationSteps(myGraph, points, x, y, scalar) {
     for (let i = 0; i < binary.length; i += 1) {
         if (binary[i] === '1') {
             arrayPowerOfTwo.push(`\\(1 \\cdot 2^{${binary.length - 1 - i}}\\) `);
-            arrayPowerOfTwoOnes.push(`\\(2^{${binary.length - 1 - i}}\\) `);
+            arrayPowerOfTwoOnes.push(`\\(2^{${binary.length - 1 - i}}\\)`);
         } else {
             arrayPowerOfTwo.push(`\\(0 \\cdot 2^{${binary.length - 1 - i}}\\) `);
         }
@@ -42,35 +42,46 @@ function pointMultiplicationSteps(myGraph, points, x, y, scalar) {
 
     document.getElementById('calculatingHeader').innerHTML = 'Point Multiplication: Double and Add Algorithm';
     stepRows[0].innerHTML = `There exist faster algorithms than simply computing \\(n \\cdot P\\), which requires n additions. One of them is the double and add algorithm: <br>
-                              Get the binary representation of the scalar \\(${scalar}\\), which is \\(\\textbf{${binary}}\\). This binary representation can be turned into a sum of powers of two: <br>`;
+                              Get the binary representation of the scalar \\(${scalar}\\), which is \\(\\textbf{${binary}}\\). This binary representation can be turned into a sum of powers of two: <br>
+                              \\(${scalar} = \\) &nbsp`;
 
-    arrayPowerOfTwo.forEach(number => stepRows[0].innerHTML += number);
-    stepRows[0].innerHTML += '<br> <br> Which we can write as: <br>';
+    for (let i = 0; i < arrayPowerOfTwo.length; i += 1) {
+        stepRows[0].innerHTML += arrayPowerOfTwo[i];
+        if (i !== arrayPowerOfTwo.length - 1) {
+            stepRows[0].innerHTML += ' + ';
+        }
+    }
+
+
+    stepRows[0].innerHTML += `<br> <br> Which we can write as: <br>
+                              \\(${scalar} \\cdot P = \\) &nbsp`;
 
     for (let i = 0; i < arrayPowerOfTwoOnes.length; i += 1) {
-        stepRows[0].innerHTML += arrayPowerOfTwoOnes[i];
+        stepRows[0].innerHTML += arrayPowerOfTwoOnes[i] + `\\(P\\)`;
         if (i !== arrayPowerOfTwoOnes.length - 1) {
             stepRows[0].innerHTML += ' + ';
         }
     }
 
-    stepRows[0].innerHTML += `<br> Therefore the Double and Add algorithm tells us to: <br> Take \\(P\\) <br>`;
+    stepRows[0].innerHTML += `<br> Therefore the Double and Add algorithm tells us to: <br> <br>• Take \\(P\\) <br>`;
 
     const binaryReversed = binary.toString().split('').reverse().join('');
     console.log(binaryReversed);
 
     for(let i = 0; i < binary.length; i += 1) {
         if(i !== 0) {
-            stepRows[0].innerHTML += `Double \\( 2^{${i - 1}}P\\), so that we get \\(2^{${i}}P\\) <br>`;
+            stepRows[0].innerHTML += `• Double \\( 2^{${i - 1}}P\\), so that we get \\(2^{${i}}P\\) <br>`;
             if (binary[binary.length - 1 - i] === '1') {
                 console.log('Index: ', binary.length - 1 - i);
-                stepRows[0].innerHTML += `Add \\(2^{${i}}P\\) to our result <br>`;
+                stepRows[0].innerHTML += `• Add \\(2^{${i}}P\\) to our result <br>`;
             } else {
-                stepRows[0].innerHTML += `Don't perform any addition involving \\(2^{${i}}P\\) <br>`;
+                stepRows[0].innerHTML += `• Don't perform any addition involving \\(2^{${i}}P\\) <br>`;
             }
         }
 
     }
+
+    stepRows[0].innerHTML += `<br>Thus, to compute \\(${scalar} \\cdot P \\), we only have to perform ${arrayPowerOfTwoOnes.length - 1} additions and ${binary.length - 1} doublings.`
 
 
 
