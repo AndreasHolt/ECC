508e95e0fa16e84052f7bc2c10c8026a7662c73b
MikkelBjørn <mikkel.bjoern@hotmail.com>
2022-05-02 11:19:36 +0200

    Old commits

--- src/js/finiteField/FiniteField.js
+++ src/js/finiteField/FiniteField.js
@@ -18,6 +18,10 @@ document.querySelector("#form").addEventListener("submit", (event) => {
     let modoli;
     let additionFunction;
     let createPointsFunction;
+    document.querySelectorAll("circle").forEach( (el) => {
+        el.remove();
+    });
+
     switch (event.target["curveList"].value) {
         case ("Prime 5"):
             prime = 5;
@@ -62,25 +66,20 @@ document.querySelector("#form").addEventListener("submit", (event) => {
             additionFunction = calcPointAdditionGF2;
             break;
     }
-    //curve = createCurveABCD(Math.floor(Math.random()*Math.pow(prime, power)),1,1,1, Math.pow(prime, power), modoli, additionFunction);
-    curve = createCurveABCD(118,0,0,0, Math.pow(prime, power), modoli, additionFunction);
-    
-    //curve = createCurveAXY(Math.floor(Math.random()*Math.pow(prime, power)), 1, 0, Math.pow(prime, power), modoli, additionFunction);
+    //curve = createCurveABCD(2,1,1,1, Math.pow(prime, power), modoli, additionFunction);
+    curve = createCurveAXY(Math.floor(Math.random()*Math.pow(prime, power)), 1, 0, Math.pow(prime, power), modoli, additionFunction);
 
 
-    let optionsList = [{mode:"multiplicative"},{mode:"additive"}];    
+    let optionsList = [{mode:"multiplicative"},{mode:"additive"}];
     for (let options of optionsList) {
-        //let arrayValues = createTable(curve.fieldOrder, curve.mod, options);
-        //createTableHTML(arrayValues, curve.fieldOrder, options.mode);
+        let arrayValues = createTable(curve.fieldOrder, curve.mod, options);
+        createTableHTML(arrayValues, curve.fieldOrder, options.mode);
     }
 
 
     //console.log("Subgroup for G: " + curve.calcSubGroup(curve.G));
     console.log("a: " + curve.a);
     curve.createPoints();
-    curve.G = curve.points[15];
-    console.log(`G.x: ${curve.G.x}, G.y: ${curve.G.y}.`);
-    console.log("Number of points: " + curve.points.length);
     drawPoints(curve.points, curve.fieldOrder);
     /*
     curve.points = [];
@@ -101,21 +100,20 @@ document.querySelector("#form").addEventListener("submit", (event) => {
         curve.d = 0;
     }
     curve.mod = modoli;
-    
+
     for (let options of optionsList) {
         let arrayValues = createTable(sizeOfTable, modoli, options);
         createTableHTML(arrayValues, sizeOfTable, options.mode);
     }
-    
+
     createPoints(curve, sizeOfTable, modoli);
     curve.drawDots(sizeOfTable);
 */
 });
-document.getElementById("additionForm").addEventListener("submit", (event) => {
-    event.preventDefault();
-    try {
-        let index1 = Number(event.target["index1"].value);
-        let index2 = Number(event.target["index2"].value);
+
+function pointAdditionFinite(index1, index2) {
+
+ try {
         let point1 = curve.points[index1];
         let point2 = curve.points[index2];
         let options = {};
@@ -124,21 +122,31 @@ document.getElementById("additionForm").addEventListener("submit", (event) => {
         } else {
             options.prime = false;
         }
-        
+
         let newPoint = curve.calcPointAddition(point1, point2);
-        drawPoint(newPoint, curve.fieldOrder, 5, "red");
-        drawPoint(point1,curve.fieldOrder, 5, "blue");
-        drawPoint(point2,curve.fieldOrder, 5, "yellow");
+        drawPointElement(newPoint, curve.fieldOrder, 5, "orange", true);
+
+        highlightPointTimeout(newPoint, 5, curve.fieldOrder);
+        drawPointElement(point1, curve.fieldOrder, 5, "red", true);
+        drawPointElement(point2, curve.fieldOrder, 5, "red", true);
         //drawLine(0, 16, 0, 1, curve.fieldOrder);
         if (index1 !== index2) {
             //drawLineDirect(point1, point2, 16);
         }
-        //drawLineDirectGood(point1, newPoint, options);
+        drawLineDirectGood(point1, newPoint, {"prime": curve.fieldOrder == curve.mod ? true : false});
     } catch (e) {
         console.log("Error! find selv ud af det!");
         console.log(e);
     }
+}
+
+document.getElementById("additionForm").addEventListener("submit", (event) => {
+    event.preventDefault();
+    let index1 = Number(event.target["index1"].value);
+    let index2 = Number(event.target["index2"].value);
+    pointAdditionFinite(index1, index2);
 });
+
 
 /*document.getElementById("power").addEventListener("input", () => {
     if (document.getElementById("power").value > 1) {
@@ -156,7 +164,7 @@ document.getElementById("Scalar").addEventListener("input", (event) => {
 
         let scaledPoint = curve.calcPointMultiplication(scale, point);
         highlightPointTimeout(scaledPoint, 1000, curve.fieldOrder);
-        
+
     }
 });
 
@@ -166,21 +174,21 @@ document.getElementById("scalarForm").addEventListener("submit", (event) => {
     let point = curve.points[index];
     let scale = Number(event.target["Scalar"].value);
     drawPoint(point, curve.fieldOrder, 5, "blue");
-    
+
     if (point) {
         let scaledPoint = curve.calcPointMultiplication(scale, point);
-        drawPoint(scaledPoint, curve.fieldOrder, 10, "red");
-        //drawLineDirectGood(point, scaledPoint,options);
+        drawPoint(scaledPoint, curve.fieldOrder, 5, "red");
+        drawLineDirectGood(point, scaledPoint, {"prime": curve.fieldOrder == curve.mod ? true : false});
     }
 });
 
 function drawLine (x1, x2, y1, y2, size, color = "black") {
     ctx.beginPath();
-    ctx.moveTo(x1 * canvas.width / size, canvas.height - (y1 * canvas.height / size));
-    ctx.lineTo(x2 * canvas.width / size, canvas.height - (y2 * canvas.height / size));
+    ctx.moveTo((x1 * canvas.width / size)-3, (canvas.height - (y1 * canvas.height / size)) -3);
+    ctx.lineTo((x2 * canvas.width / size)-3, (canvas.height - (y2 * canvas.height / size)) - 3);
     ctx.strokeStyle = color;
     ctx.stroke();
-    
+
     /*if (progress < curve.fieldOrder) {
         setTimeout(() => {drawLine(point1, point3, progress + 0.01)}, 0.1);
         let newPoint = {"x":Mod(point1.x + (progress),curve.fieldOrder), "y":Mod(point1.y+(point3.alfa*progress),curve.fieldOrder)};
@@ -220,6 +228,7 @@ function drawLineDirect_AUX (alfa, progress, speed, previousPoint, delay) {
 }
 
 function drawLineDirectGood (point, point3, options) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     let point1 = {x: point.x, y: point.y};
     let tempPoint = {x: point1.x, y: point1.y};
     let i = 0;
@@ -229,6 +238,8 @@ function drawLineDirectGood (point, point3, options) {
     } else if (options.prime == true) {
         point3.y = Mod(curve.fieldOrder - point3.y, curve.fieldOrder);
     }
+
+    drawPointElement(point3, curve.fieldOrder, 5, "fuchsia", true);
 
     while(((tempPoint.x != point3.x) || (tempPoint.y != point3.y)) && i < 100) {
         tempPoint.x += 1;
@@ -265,7 +276,7 @@ function drawLineDirectGood (point, point3, options) {
 function drawPoints (arrayPoints, fieldOrder) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let point of arrayPoints) {
-        drawPointElement(point, fieldOrder, 5, "black");
+        drawPointElement(point, fieldOrder, 5, "rgb(59,129,246)");
     }
 }
 
@@ -288,6 +299,7 @@ function highlightPointTimeout (point, time, size) {
     circle.setAttributeNS(null, 'cy', y);
     circle.setAttributeNS(null, 'r', 10);
     circle.setAttributeNS(null, 'style', 'fill: none; stroke: blue; stroke-width: 1px;' );
+    circle.style.zIndex = '50000';
     svg.appendChild(circle);
     setTimeout(() => {
         svg.removeChild(circle);
@@ -305,17 +317,105 @@ function highlightPoint (point, size) {
     return circle;
 }
 
-function drawPointElement (point, size, pointSize, color) {
+function pointDescription(point) {
+    let negation, subGroupPoint = point;
+    let subGroup = [];
+    let orderOfSubGroup = 0;
+
+    for (let i of curve.points) {
+        if (i.x === point.x && i.y !== point.y) {
+            negation = i;
+        }
+    }
+
+    while (subGroupPoint.x !== negation.x || subGroupPoint.y !== negation.y) {
+        subGroupPoint = curve.calcPointAddition(subGroupPoint, point);
+        subGroup[orderOfSubGroup] = subGroupPoint;
+        orderOfSubGroup++;
+        
+    }
+
+    console.log(subGroup)
+
+    return {negation, subGroup, orderOfSubGroup};
+}
+
+
+function drawPointElement (point, size, pointSize, color, temp = false) {
     let svg = document.getElementById("highlightSVG");
     var svgns = "http://www.w3.org/2000/svg";
     var circle = document.createElementNS(svgns, 'circle');
     circle.setAttributeNS(null, 'cx', point.x * canvas.width / size);
     circle.setAttributeNS(null, 'cy', canvas.height - (point.y * canvas.height / size));
-    circle.setAttributeNS(null, 'r', pointSize);
+    circle.setAttributeNS(null, 'r', pointSize);                                                             //(canvas.height / (curve.fieldOrder * 1.2)) <= 5 ? (canvas.height / (curve.fieldOrder * 1.2)) : 5)
     circle.setAttributeNS(null, 'style', `fill: ${color}; stroke: ${color}; stroke-width: 1px;` );
     svg.appendChild(circle);
 
+    console.log("ab: ", curve.a,curve.b)
+
+
+
+    
     circle.addEventListener("click", () => {
+        ctx.clearRect(0, 0, 600, 600);
+        let clickedPoints = document.getElementsByClassName('clickedPoint')
+        console.log('Clicked points: ', clickedPoints)
+        let circles = document.querySelectorAll('circle')
+
+
+        circles.forEach(circle => {
+            if(!circle.classList.contains('clickedPoint')) {
+                circle.setAttributeNS(null, 'style', 'fill: rgb(59,129,246); stroke: rgb(59,129,246); stroke-width: 1px;');
+            } else if(circle.hasAttribute('pointerEvents')) {
+                circle.remove();
+            }
+
+        });
+        
+        let indexOfClickedPoints = [];
+
+
+        if(clickedPoints.length ===  2) {
+            for(let i = clickedPoints.length - 1; i >= 0; i--) {
+                console.log(clickedPoints[i].value)
+                clickedPoints[i].setAttributeNS(null, 'style', 'fill: rgb(59,129,246); stroke: rgb(59,129,246); stroke-width: 1px;');
+                clickedPoints[i].classList.remove('clickedPoint')
+            }
+
+        }
+        circle.classList.add('clickedPoint')
+
+        if(clickedPoints.length === 2) {
+            for(let i = 0; i < document.querySelectorAll('circle').length; i++) {
+                if(circles[i].classList.contains('clickedPoint')){
+                    console.log('Pushing: ', i)
+                    indexOfClickedPoints.push(i);
+                }
+
+            }
+            
+
+            console.log('Calling point addition on indexes: ', indexOfClickedPoints)
+
+            pointAdditionFinite(indexOfClickedPoints[0], indexOfClickedPoints[1])
+
+        }
+
+
+        circle.setAttributeNS(null, 'style', 'fill: red; stroke: red; stroke-width: 1px;' );
+
+        let pointDetailArray = pointDescription(point);
+
+
+        let orderOfSubGroupString = `(${point.x}, ${point.y}), `;
+        for(let i = 0; i < pointDetailArray.orderOfSubGroup; i++) {
+            console.log(i)
+            orderOfSubGroupString += `(${pointDetailArray.subGroup[i].x}, ${pointDetailArray.subGroup[i].y}) → `;
+
+        }
+
+        orderOfSubGroupString += `∞`;
+
         let output = document.getElementById("pointInfo");
         let index;
         curve.points.forEach((elem, i) => {
@@ -323,18 +423,55 @@ function drawPointElement (point, size, pointSize, color) {
                 index = i;
             }
         });
-        let string = `Point x: ${point.x}, Point y: ${point.y}, Point index: ${index}`;
-        output.textContent = string;
+
+
+        let pointDetails = [`<span class="detailKey">Index:</span> ${index}`, `<span class="detailKey">Point:</span> (${point.x}, ${point.y})`, `<span class="detailKey">Inverse:</span> (${pointDetailArray.negation.x}, ${pointDetailArray.negation.y})`, `<span class="detailKey">Subgroup: </span> ${pointDetailArray.orderOfSubGroup + 2}`,`<span class="detailKey">Generated sub group: </span> ${orderOfSubGroupString}`];
+
+        output.innerHTML = "";
+
+        for(let i = 0; i < pointDetails.length; i++) {
+            let p = document.createElement("p");
+            output.appendChild(p);
+            p.setAttribute("class", "pointDetails");
+
+            p.innerHTML += pointDetails[i];
+
+            let detailKey = document.getElementsByClassName("detailKey");
+            detailKey[i].classList.add("tracking-wide", "text-gray-700", "text-x", "font-bold", "mb-2")
+
+
+        }
+
+        //svg.appendChild(pointText(point, temp));
     });
+    
     circle.addEventListener("mouseover", () => {
         highlightPointTimeout(point, 400, curve.fieldOrder);
     });
 
-    
+    if(temp) {
+        circle.style.pointerEvents = "none"
+    }
 }
 
 
+function pointText (point, eq = "") {
+    let textNode;
+    const textElement = document.createElementNS('http://www.w3.org/2000/svg', 'text');
 
+    textElement.setAttribute('x', point.x * canvas.width / curve.fieldOrder);
+    textElement.setAttribute('y', canvas.height - (point.y * canvas.height / curve.fieldOrder));
+
+    if (eq) {
+        textNode = document.createTextNode(eq + `(${point.x}, ${point.y})`);
+        //textElement.setAttribute('id', eq);
+    } else {
+        textNode = document.createTextNode(`(${point.x}, ${point.y})`);
+    }
+
+    textElement.appendChild(textNode);
+    return textElement;
+}
 
 
 
@@ -346,30 +483,51 @@ function createTableHTML (tableArray, tableSize, htmlID) {
     newTable.id = htmlID;
 
     let headerRow = document.createElement("tr");
+
     for (let i = -1; i < tableSize; i++) {
         let header = document.createElement("th");
+
+
         if (i !== -1) {
             header.textContent = i;
+            header.classList.add("text-sm", "text-white", "font-medium", "px-2", "py-2", "whitespace-nowrap")
+
         } else {
+            header.classList.add("border-b", "bg-gray-800", "border-gray-800")
+
+            header.classList.add("text-sm", "text-white", "font-medium", "px-6", "py-6", "whitespace-nowrap")
+            newTable.classList.add("inline-block", "relative", "w-64", "basis-1/2", "table-fixed")
+
             if (htmlID === "multiplicative") {
                 header.textContent = "*";
+
+
             } else if (htmlID === "additive"){
                 header.textContent = "+";
+
             }
         }
+        headerRow.classList.add("border-b", "bg-gray-800", "border-gray-900")
+
         headerRow.appendChild(header);
     }
     newTable.appendChild(headerRow);
 
+
     //For each row
     for (let rowIndex = 0; rowIndex < tableSize; rowIndex++) {
         let row = document.createElement("tr");
+
         let dataCell = document.createElement("td");
         dataCell.textContent = rowIndex;
+
         row.appendChild(dataCell);
+
+        row.firstChild.classList.add("border-b", "bg-gray-800", "border-gray-800", "text-sm", "text-white", "font-medium", "whitespace-nowrap", "text-center")
         for (let i = 0; i < tableSize; i++) {
             let dataCell = document.createElement("td");
             dataCell.textContent = tableArray[i][rowIndex];
+            dataCell.classList.add('mx-10', 'my-100')
             row.appendChild(dataCell);
         }
         newTable.appendChild(row);
@@ -378,7 +536,7 @@ function createTableHTML (tableArray, tableSize, htmlID) {
     if (oldTable) {
         oldTable.replaceWith(newTable);
     } else {
-        document.getElementById("outputTableColumn").after(newTable);
+        document.getElementById("outputTableColumn").appendChild(newTable);
     }
 
 }
@@ -388,11 +546,11 @@ function createTable (sizeOfTable, mod, options) {
     let arrayValues = [];
     if (options.mode === "additive") {
         arrayValues = calculateElements(sizeOfTable, mod, additiveXOR);
-        
+
     } else if (options.mode === "multiplicative") {
         arrayValues = calculateElements(sizeOfTable, mod, multiplicativeXOR);
     }
-    
+
     return arrayValues;
 }
 
@@ -422,23 +580,18 @@ function calculateElements(size, mod, combinationFunction) {
 
 
 
-
-
-
-
-
 function gcd (val1, val2) {
 
 }
 
 function calcIreduciblePoly (size) {
 
-    
+
 }
 
 
 function isPrime(val){
-    
+
 }
 
 
@@ -450,7 +603,7 @@ function showTable(arrayValues) {
 function clmul32(x1, x2, mod) {         //https://www.youtube.com/watch?v=v4HKU_VBbqM
     let result = 0;
     //x2 |= 0;                            //Kun nødvendig hvis der er chance for at input er double
-    
+
     while (x1 !== 0) {
         result ^= x2 * (x1 & -x1);
         x1 &= x1 - 1;
