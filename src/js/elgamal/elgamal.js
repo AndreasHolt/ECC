203e12a49c084170d86f59763bebd03f0d52078e
Theis Mathiassen <theis.mathiassen@gmail.com>
2022-05-06 14:20:49 +0200

    Old commits

--- src/js/elgamal/elgamal.js
+++ src/js/elgamal/elgamal.js
@@ -104,7 +104,7 @@ class User {
         let pointResult = [];
         let numberResult = [];
         let numPoints = BigInt(curve.points.length);
-        let blockSize = estLog2BigIntFloor(numPoints)/estLog2BigIntFloor(charSize-1n);    //log__base(points)
+        let blockSize = estLog2BigIntFloor(numPoints)/estLog2BigIntFloor(charSize);    //log__base(points)
         if (blockSize < 1) {
             throw("Not enough points on curve");
         }
@@ -257,7 +257,7 @@ function estLog2BigIntCeil(bigInt) {
 }
 
 function estLog2BigIntFloor(bigInt) {
-    return BigInt(bigInt.toString(2).length);
+    return BigInt(bigInt.toString(2).length-1);
 }
 
 
